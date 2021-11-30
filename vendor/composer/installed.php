53d7df4546f21ff811e222a9d212b435473b8c2b
= <=>
2021-11-30 09:54:07 +0600

    init 2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '71c18926b9ebce7138f934613450e45097155e74',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '71c18926b9ebce7138f934613450e45097155e74',
+    ),
+  ),
+);
